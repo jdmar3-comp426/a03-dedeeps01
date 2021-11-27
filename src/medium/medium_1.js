dd6fc6d6835daa10484d09b1c16d85d36654c14f
Dedeepya Madineni <dedeepya01@yahoo.com>
2021-11-27 18:49:49 -0500

    getMedian name fix

--- src/medium/medium_1.js
+++ src/medium/medium_1.js
@@ -26,12 +26,12 @@ export function getSum(array) {
  * console.log(getMedian(array)); // 4.5
  */
 export function getMedian(array) {
-    for(let i = 0; i < arr.length; i++){
-        for(let j = 0; j < ( arr.length - i -1 ); j++){
-          if(arr[j] > arr[j+1]){
-            let temp = arr[j]
-            arr[j] = arr[j + 1]
-            arr[j+1] = temp
+    for(let i = 0; i < array.length; i++){
+        for(let j = 0; j < ( array.length - i -1 ); j++){
+          if(arr[j] > array[j+1]){
+            let temp = array[j]
+            array[j] = array[j + 1]
+            array[j+1] = temp
           }
         }
       }
