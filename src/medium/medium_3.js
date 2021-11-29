de5ef16f77f01e223f0992cfd89ce1f1523e4cdb
Dedeepya Madineni <dedeepya01@yahoo.com>
2021-11-29 15:32:16 -0500

    medium_3 submit

--- src/medium/medium_3.js
+++ src/medium/medium_3.js
@@ -18,7 +18,17 @@ queries.
  *
  */
 export function searchHighPower(car_data, minHorsepower, minTorque) {
-
+    car_data.filter(car => car['horsepower'] >= minHorsepower && car['torque'] >= minTorque)
+    for(let i = 0; i < car_data.length; i++) {
+        for(let j = 0; j < ( car_data.length - i -1 ); j++){
+          if(car_data[j]['horsepower'] < car_data[j+1]['horsepower']){
+            var temp = car_data[j]
+            car_data[j] = car_data[j + 1]
+            car_data[j+1] = temp
+          }
+        }
+      }
+      return car_data
 }
 
 
@@ -33,7 +43,17 @@ export function searchHighPower(car_data, minHorsepower, minTorque) {
  *
  */
 export function searchMpg(car_data, minCity, minHighway) {
-
+    car_data.filter(car => car['highway_mpg'] >= minHighway && car['city_mpg'] >= minCity)
+    for(let i = 0; i < car_data.length; i++) {
+        for(let j = 0; j < ( car_data.length - i -1 ); j++){
+          if(car_data[j]['highway_mpg'] < car_data[j+1]['highway_mpg']){
+            var temp = car_data[j]
+            car_data[j] = car_data[j + 1]
+            car_data[j+1] = temp
+          }
+        }
+      }
+    return car_data
 }
 
 
@@ -46,7 +66,16 @@ export function searchMpg(car_data, minCity, minHighway) {
  * @returns {[]} array of cars
  */
 export function searchName(car_data, searchTerm) {
-
+    let ordered = new Array()
+    car_data.filter(car => car['id'].toLowerCase().includes(searchTerm.toLowerCase()))
+    for(let i in car_data.length) {
+        for(let j in car_data) {
+            if(car_data[j]['id'].indexof(searchTerm) === i) {
+                ordered.push(car_data[j])
+            } 
+        }
+    }
+    return ordered
 }
 
 
@@ -59,5 +88,15 @@ export function searchName(car_data, searchTerm) {
  * @returns {[]} an array of car objects
  */
 export function searchByYear(car_data, years) {
-
+    car_data.filter(car => years.includes(car['year']))
+    for(let i = 0; i < car_data.length; i++) {
+        for(let j = 0; j < ( car_data.length - i -1 ); j++){
+          if(car_data[j]['year'] < car_data[j+1]['year']){
+            var temp = car_data[j]
+            car_data[j] = car_data[j + 1]
+            car_data[j+1] = temp
+          }
+        }
+      }
+    return car_data
 }
